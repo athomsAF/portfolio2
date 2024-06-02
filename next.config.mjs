1333899e6e4ad40a731bef88d991c203fb3da58a
Brioche <thomasderudder@sfr.fr>
2024-06-02 14:06:54 +0200

    shadcn & velite

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,24 @@
-/** @type {import('next').NextConfig} */
-// alias
-import nextMDX from "@next/mdx";
+import { build } from "velite";
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
+/** @type {import('next').NextConfig} */
+export default {
+  // othor next config here...
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
+    return config;
   },
-});
-
-/** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
 };
-export default withMDX(nextConfig);
+
+class VeliteWebpackPlugin {
+  static started = false;
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+      if (VeliteWebpackPlugin.started) return;
+      VeliteWebpackPlugin.started = true;
+      const dev = compiler.options.mode === "development";
+      await build({ watch: dev, clean: !dev });
+    });
+  }
+}

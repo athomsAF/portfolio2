95f7e8742c6b5437461e5c97d4836613b980d03b
Brioche <thomasderudder@sfr.fr>
2024-06-02 16:31:32 +0200

    setup data

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import { Github } from "lucide-react";
+
+export const siteConfig = {
+  name: "Bricohe",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Brioche",
+  links: {
+    github: "https://github.com/athomsAF",
+    personnalSite: "https://kodelab.fr",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
